d38093115642be4f4bc737521377c00524af2311
kpango <i.can.feel.gravity@gmail.com>
2019-02-20 02:39:29 +0900

    fix

--- fuid_test.go
+++ fuid_test.go
@@ -1,6 +1,8 @@
 package fuid
 
 import (
+	"context"
+	"sync"
 	"testing"
 )
 
@@ -33,6 +35,58 @@ func TestFUID_String(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := New().String(); got == "" {
 				t.Errorf("FUID.String() = %v", got)
+			}
+		})
+	}
+}
+
+func TestFUIDCollisionRate(t *testing.T) {
+	tests := []struct {
+		name     string
+		routines int
+		cycle    int
+		buf      int
+	}{
+		{
+			name:     "normal",
+			routines: 100,
+			cycle:    100000,
+			buf:      100000,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ch := make(chan string, tt.buf)
+			wg := new(sync.WaitGroup)
+			wg.Add(1)
+			go func() {
+				for i := 0; i < tt.routines; i++ {
+					wg.Add(1)
+					go func() {
+						ch <- String()
+						wg.Done()
+					}()
+				}
+				wg.Done()
+			}()
+			ctx, cancel := context.WithCancel(context.Background())
+			go func() {
+				wg.Wait()
+				cancel()
+			}()
+			check := map[string]bool{}
+			counter := 0
+			for {
+				select {
+				case <-ctx.Done():
+					t.Log(counter)
+				case str := <-ch:
+					if check[str] == true {
+						counter++
+					} else {
+						check[str] = true
+					}
+				}
 			}
 		})
 	}
